3c4805f1051c236e7b9afd220cf6d786148c08c5
Ciasterix <cwaligora97@gmail.com>
2020-03-18 22:40:51 +0100

    changed evaluator fields to public

--- zad1/src/evaluator.py
+++ zad1/src/evaluator.py
@@ -9,11 +9,11 @@ Solution = List[int]
 class Evaluator:
 
     def __init__(self):
-        self._min_val = None
-        self._min_solution = None
-        self._max_val = None
-        self._max_solution = None
-        self._mean_val = None
+        self.min_val = None
+        self.min_solution = None
+        self.max_val = None
+        self.max_solution = None
+        self.mean_val = None
 
     def evaluate(self, solver: Solver, iterations: int) -> None:
         results = []
@@ -21,13 +21,13 @@ class Evaluator:
             results.append(solver.solve(start_idx = i))
         solutions = np.array(results)
         solutions = solutions[np.argsort(solutions[:, 1])]
-        self._min_solution = solutions[0, 0]
-        self._min_val = solutions[0, 1]
-        self._max_solution = solutions[-1, 0]
-        self._max_val = solutions[-1, 1]
-        self._mean_val = solutions[:, 1].mean()
+        self.min_solution = solutions[0, 0]
+        self.min_val = solutions[0, 1]
+        self.max_solution = solutions[-1, 0]
+        self.max_val = solutions[-1, 1]
+        self.mean_val = solutions[:, 1].mean()
 
     def print_metrics(self) -> None:
-        print(f'Shortest path length: {self._min_val}')
-        print(f'Longest path length: {self._max_val}')
-        print(f'Mean path length: {self._mean_val}')
+        print(f'Shortest path length: {self.min_val}')
+        print(f'Longest path length: {self.max_val}')
+        print(f'Mean path length: {self.mean_val}')
