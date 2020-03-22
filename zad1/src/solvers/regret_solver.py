15fbb0e7666ac150179e25dbad96f25728495796
Ciasterix <cwaligora97@gmail.com>
2020-03-23 00:16:40 +0100

    Comments deletion and propper length calculation

--- zad1/src/solvers/regret_solver.py
+++ zad1/src/solvers/regret_solver.py
@@ -6,6 +6,7 @@ from solvers.solver import Solver
 
 Solution = List[int]
 
+
 class RegretSolver(Solver):
 
     def __init__(self, matrix: np.ndarray):
@@ -16,11 +17,8 @@ class RegretSolver(Solver):
         self.status[vert_idx] = False
 
     def delete_node(self, position) -> None:
-        # print(f'position d: {position}')
-        # print(f'solution d: {self.solution}')
         if position >= len(self.solution):
             position = -1
-        # st2idx = np.where(np.logical_not(self.status))[0]
         self.status[self.solution[position]] = True
         del self.solution[position]
 
@@ -32,11 +30,11 @@ class RegretSolver(Solver):
         self.insert_node(0, first_idx)
         second_idx = self.find_nearest_vertex(first_idx)
         self.insert_node(1, second_idx)
-        self.length = 0
+        self.length = 2 * (self._matrix[first_idx, second_idx])
 
     def find_nearest_vertex(self, idx: int) -> int:
         # Vector of distances from first node
-        x = self._matrix[idx] 
+        x = self._matrix[idx]
         # Indices of unused nodes
         st2idx = np.where(self.status)[0]
         vert_idx = st2idx[np.argmin(x[st2idx])]
@@ -65,12 +63,9 @@ class RegretSolver(Solver):
         )
         return idx_min[1]+1, st2idx[idx_min[0]], len_changes[idx_min]
 
-    def find_alternative_vertex(self, replaced_idx) -> None:
-        # print('-'*70)
+    def calculate_regret(self, replaced_idx) -> None:
         tmp_status = []
         tmp_status = self.status.copy()
-        # print(tmp_status)
-        # print(replaced_idx)
         if replaced_idx >= len(self.solution):
             tmp_status[self.solution[-1]] = True
             tmp_status[self.solution[0]] = True
@@ -78,27 +73,16 @@ class RegretSolver(Solver):
             tmp_status[self.solution[replaced_idx-1]] = True
             tmp_status[self.solution[replaced_idx]] = True
         st2idx = np.where(np.logical_not(tmp_status))[0]
-        # print(f'st2idx: {st2idx}')
         edges_zipped = zip(
             self.solution,
             [self.solution[-1]] + self.solution,
             self.solution[1:] + [self.solution[0]]
         )
         edges = np.array(list(map(np.array, edges_zipped)))
-        # print(f'Edges before deletion: {edges}')
         if replaced_idx >= len(self.solution):
             edges = edges[1:-1]
         else:
-            # edges = np.delete(edges, [replaced_idx-1], axis=0)
             edges = np.delete(edges, [replaced_idx-1, replaced_idx], axis=0)
-        # print(f'Edges after deletion: {edges}')
-        # tmp_mat = np.array(
-        #     [
-        #         self._matrix[edges[:, 0], edges[:, 1]],
-        #         self._matrix[edges[:, 0], edges[:, 2]]
-        #     ]
-        # )
-        # print(f'tmp_mat_print: \n{tmp_mat}')
         added_lengths = np.sum(
             [
                 self._matrix[edges[:, 0], edges[:, 1]],
@@ -106,42 +90,32 @@ class RegretSolver(Solver):
             ],
             axis=0
         )
-        # print(f'added_lengths: {added_lengths}')
         edges_lengths = np.array(
             self._matrix[edges[:, 1], edges[:, 2]]
         )
-        # print(f'edges_lengths: {edges_lengths}')
         len_changes = added_lengths - edges_lengths
-        # print(f'len_changes: {len_changes}')
-        # idx_min = np.unravel_index(
-        #     np.argmin(len_changes, axis=None),
-        #     len_changes.shape
-        # )
         idx_min = np.argmin(len_changes)
         vertex = edges[idx_min, 0]
-        # self.length += len_changes[idx_min]
         return idx_min, vertex, len_changes[idx_min]
 
     def solve(self, start_idx: int = 0) -> Tuple[Solution, int]:
         self.insert_first_nodes(start_idx)
 
+        # insert third node
         position, vertex_idx, len_change = self.find_next_vertex()
         self.insert_node(position, vertex_idx)
+        self.length += len_change
 
-        # for _ in range(3):  # range(ceil((len(self.status))/2)-2):
-        while len(self.solution) < 50:  # position, vertex_idx, len_change = self.find_next_vertex()
+        while len(self.solution) < ceil(len(self.status)/2):
             position, vertex_idx, len_change = self.find_next_vertex()
-            # print(f'self.solution{self.solution}')
-            # print(f'position: {position}')
-            pos2, v_idx2, len_change_del = self.find_alternative_vertex(position)
-
-            if len_change >= len_change_del:
+            position2, _, regret = self.calculate_regret(position)
+            if len_change >= regret:
                 self.insert_node(position, vertex_idx)
                 self.length += len_change
             else:
-                self.delete_node(pos2)
+                self.delete_node(position2)
                 self.insert_node(position, vertex_idx)
-                self.length -= len_change_del
-                self.length += len_change
+                self.length -= regret
+                self.length += regret
 
         return self.solution, self.length
