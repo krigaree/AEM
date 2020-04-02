224c7d675dee1fe82e6af239f07100af5caa17d9
krigaree <piotrwyrwinski97@gmail.com>
2020-04-02 22:25:43 +0200

    First local search algorithm - 2-opt

---
+++ zad2/src/two_opt.py
@@ -0,0 +1,54 @@
+from copy import deepcopy
+from typing import List
+
+import numpy as np
+
+Tour = List[int]
+
+class TwoOpt:
+    """Local search tour improvement algorithm.
+    It consider every possible 2-edges swap,
+    swapping 2 edges when it results in an improved tour.
+    """
+
+    def __init(self) -> None:
+        pass
+
+
+    def swap(self, tour: Tour, i: int, j: int) -> Tour:
+        """It take tour like [1,2,3,4,5,6] and indices 2 and 4
+        and reverse sub_tour [3,4,5] between those two indices.
+        Returned tour is [1,2,5,4,3,6]"""
+
+        new_tour = deepcopy(tour)
+        new_tour[i:j+1] = list(reversed(new_tour[i:j+1]))
+        return new_tour
+
+
+    def improve(self, tour: Tour, matrix: np.ndarray) -> Tour:
+        """We want to iterate until there is no improvement."""
+
+        break_flag = True # If new candidate is found dont break loop
+        i=0
+        while break_flag:
+            i+=1
+            break_flag = False
+            for node_i in range(0, len(tour) - 1):
+                for node_j in range(node_i+1, len(tour) - 1):
+                    if node_i!=0 or node_j!=len(tour)-1:
+                        first_edges = matrix[tour[node_i-1], tour[node_i]]\
+                            + matrix[tour[node_j], tour[node_j+1]]
+                        second_edges = matrix[tour[node_i-1], tour[node_j]]\
+                            + matrix[tour[node_i], tour[node_j+1]]
+                        new_delta =  second_edges - first_edges
+                        if new_delta<0:
+                            if node_i<1 or new_delta < delta:
+                                delta: int = new_delta
+                                candidate = (node_i, node_j)
+                                break_flag = True
+            tour = self.swap(tour, candidate[0], candidate[1])
+
+        return tour
+
+
+
---
+++ zad2/src/visualizer.py
@@ -0,0 +1,45 @@
+from typing import List, Union
+import numpy as np
+import matplotlib.pyplot as plt
+import networkx as nx
+
+Solution = List[int]
+Vertex = List[int]
+
+
+class Visualizer:
+
+    def __init__(self):
+        self.graph = None
+        self.graph_image = None
+
+    def create_graph(self, solution: Solution, matrix: np.ndarray) -> None:
+        rows, cols = np.where(matrix > 0)
+        points_a = solution.copy()[:-1]
+        points_b = solution.copy()[1:]
+        edges = zip(points_a, points_b)
+        gr = nx.Graph()
+        gr.add_edges_from(edges)
+        nx.draw(gr, node_size=100)
+        plt.show()
+
+    def create_graph_euclidean(
+            self, solution: Solution, matrix: np.ndarray,
+            vertices: Union[np.ndarray, List[Vertex]]
+    ) -> None:
+
+        vertices = np.array(vertices)
+        plt.plot(vertices[:, 0], vertices[:, 1], "o")
+        points_a = solution.copy()
+        points_b = np.concatenate([solution.copy()[1:], solution[:1]])
+        points_a = vertices[points_a]
+        points_b = vertices[points_b]
+        for a, b in zip(points_a, points_b):
+            plt.plot([a[0], b[0]], [a[1], b[1]], '-')
+        for n in range(len(vertices)):
+            plt.annotate(str(n), (vertices[n, 0], vertices[n, 1]))
+        plt.savefig("wykres.png")
+        plt.show()
+
+    def save_graph(self, graph=None) -> None:
+        raise NotImplementedError
