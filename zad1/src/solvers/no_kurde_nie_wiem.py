7927a1475b646d5cdf8eb24f7fedce18924670cb
Ciasterix <cwaligora97@gmail.com>
2020-04-06 15:15:04 +0200

    old files deleted

---
+++ zad1/src/solvers/no_kurde_nie_wiem.py
@@ -0,0 +1,109 @@
+------------------------------------------------------------
+v len_change: 275.0
+v: 89
+replaced_idx: 4
+self.solution: [0, 91, 7, 74, 48, 5, 62]
+st2idx: [ 0  5  7 62 91]
+edges before delete: [[ 0 62 91]
+ [91  0  7]
+ [ 7 91 74]
+ [74  7 48]
+ [48 74  5]
+ [ 5 48 62]
+ [62  5  0]]
+edges after delete: [[ 0 62 91]
+ [91  0  7]
+ [ 7 91 74]
+ [ 5 48 62]
+ [62  5  0]]
+added_lengths: [591. 509. 663. 204. 445.]
+edges_lengths: [353. 425. 358. 202. 397.]
+len_changes: [238.  84. 305.   2.  48.]
+idx_min: 2
+vertex: 7
+D E L E T E
+solution before delete: [0, 91, 7, 74, 48, 5, 62]
+position: 2
+solution after delete: [0, 91, 74, 48, 5, 62]
+------------------------------------------------------------
+v len_change: 305.0
+v: 7
+replaced_idx: 2
+self.solution: [0, 91, 74, 48, 89, 5, 62]
+st2idx: [ 0  5 48 62 89]
+edges before delete: [[ 0 62 91]
+ [91  0 74]
+ [74 91 48]
+ [48 74 89]
+ [89 48  5]
+ [ 5 89 62]
+ [62  5  0]]
+edges after delete: [[ 0 62 91]
+ [48 74 89]
+ [89 48  5]
+ [ 5 89 62]
+ [62  5  0]]
+added_lengths: [591. 557. 457. 406. 445.]
+edges_lengths: [353. 416.  47. 374. 397.]
+len_changes: [238. 141. 410.  32.  48.]
+idx_min: 2
+vertex: 89
+D E L E T E
+solution before delete: [0, 91, 74, 48, 89, 5, 62]
+position: 4
+solution after delete: [0, 91, 74, 48, 5, 62]
+------------------------------------------------------------
+v len_change: 275.0
+v: 89
+replaced_idx: 4
+self.solution: [0, 91, 7, 74, 48, 5, 62]
+st2idx: [ 0  5  7 62 91]
+edges before delete: [[ 0 62 91]
+ [91  0  7]
+ [ 7 91 74]
+ [74  7 48]
+ [48 74  5]
+ [ 5 48 62]
+ [62  5  0]]
+edges after delete: [[ 0 62 91]
+ [91  0  7]
+ [ 7 91 74]
+ [ 5 48 62]
+ [62  5  0]]
+added_lengths: [591. 509. 663. 204. 445.]
+edges_lengths: [353. 425. 358. 202. 397.]
+len_changes: [238.  84. 305.   2.  48.]
+idx_min: 2
+vertex: 7
+D E L E T E
+solution before delete: [0, 91, 7, 74, 48, 5, 62]
+position: 2
+solution after delete: [0, 91, 74, 48, 5, 62]
+------------------------------------------------------------
+v len_change: 305.0
+v: 7
+replaced_idx: 2
+self.solution: [0, 91, 74, 48, 89, 5, 62]
+st2idx: [ 0  5 48 62 89]
+edges before delete: [[ 0 62 91]
+ [91  0 74]
+ [74 91 48]
+ [48 74 89]
+ [89 48  5]
+ [ 5 89 62]
+ [62  5  0]]
+edges after delete: [[ 0 62 91]
+ [48 74 89]
+ [89 48  5]
+ [ 5 89 62]
+ [62  5  0]]
+added_lengths: [591. 557. 457. 406. 445.]
+edges_lengths: [353. 416.  47. 374. 397.]
+len_changes: [238. 141. 410.  32.  48.]
+idx_min: 2
+vertex: 89
+D E L E T E
+solution before delete: [0, 91, 74, 48, 89, 5, 62]
+position: 4
+solution after delete: [0, 91, 74, 48, 5, 62]
+------------------------------------------------------------
