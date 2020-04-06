786494b36bb4b97d3ba879cac3b6850d58e8d143
Ciasterix <cwaligora97@gmail.com>
2020-04-06 12:58:59 +0200

    Sketch of new project structure

---
+++ tsp_router/constructive_heuristics/greedy_regret_cycle_solver.py
@@ -0,0 +1,129 @@
+from typing import List, Tuple
+import numpy as np
+from math import ceil
+
+from solvers.solver import Solver
+
+Solution = List[int]
+
+
+class GreedyRegretCycleSolver(Solver):
+
+    def __init__(self, matrix: np.ndarray):
+        super().__init__(matrix)
+
+    def insert_node(self, position: int, vert_idx: int) -> None:
+        self.solution.insert(position, vert_idx)
+        self.status[vert_idx] = False
+
+    def delete_node(self, position) -> None:
+        if position >= len(self.solution):
+            position = -1
+        self.status[self.solution[position]] = True
+        del self.solution[position]
+
+    def insert_first_nodes(self, first_idx: int) -> None:
+        # Array for storing ordered idices of used nodes
+        self.solution: Solution = []
+        # Array for storing information about unused nodes
+        self.status = [True]*len(self._matrix)
+        self.insert_node(0, first_idx)
+        second_idx = self.find_nearest_vertex(first_idx)
+        self.insert_node(1, second_idx)
+        self.length = 2 * (self._matrix[first_idx, second_idx])
+
+    def find_nearest_vertex(self, idx: int) -> int:
+        # Vector of distances from first node
+        x = self._matrix[idx]
+        # Indices of unused nodes
+        st2idx = np.where(self.status)[0]
+        vert_idx = st2idx[np.argmin(x[st2idx])]
+        return vert_idx
+
+    def find_next_vertex(self) -> Tuple[int, int, int]:
+        st2idx = np.where(self.status)[0]
+        edges_zipped = zip(
+            self.solution, self.solution[1:] + [self.solution[0]])
+        edges = np.array(list(map(np.array, edges_zipped)))
+        added_lengths = np.sum(
+            [
+                self._matrix[np.ix_(st2idx, edges[:, 0])],
+                self._matrix[np.ix_(st2idx, edges[:, 1])]
+            ],
+            axis=0
+        )
+        edges_lengths = np.tile(
+            self._matrix[edges[:, 0], edges[:, 1]],
+            (added_lengths.shape[0], 1)
+        )
+        len_changes = added_lengths - edges_lengths
+        idx_min = np.unravel_index(
+            np.argmin(len_changes, axis=None),
+            len_changes.shape
+        )
+        return idx_min[1]+1, st2idx[idx_min[0]], len_changes[idx_min]
+
+    def calculate_regret(self, replaced_idx) -> Tuple[int, int, int]:
+        tmp_status = self.status.copy()
+        if replaced_idx >= len(self.solution):
+            tmp_status[self.solution[-1]] = True
+            tmp_status[self.solution[0]] = True
+        else:
+            tmp_status[self.solution[replaced_idx-1]] = True
+            tmp_status[self.solution[replaced_idx]] = True
+        st2idx = np.where(np.logical_not(tmp_status))[0]
+        edges_zipped = zip(
+            self.solution,
+            [self.solution[-1]] + self.solution,
+            self.solution[1:] + [self.solution[0]]
+        )
+        edges = np.array(list(map(np.array, edges_zipped)))
+        if replaced_idx >= len(self.solution):
+            edges = edges[1:-1]
+        else:
+            edges = np.delete(edges, [replaced_idx-1, replaced_idx], axis=0)
+        added_lengths = np.sum(
+            [
+                self._matrix[edges[:, 0], edges[:, 1]],
+                self._matrix[edges[:, 0], edges[:, 2]]
+            ],
+            axis=0
+        )
+        edges_lengths = np.array(
+            self._matrix[edges[:, 1], edges[:, 2]]
+        )
+        len_changes = added_lengths - edges_lengths
+        idx_min = np.argmax(len_changes)
+        vertex = edges[idx_min, 0]
+        idx_max = np.where(self.solution == vertex)[0][0]
+        return idx_max, vertex, len_changes[idx_min]
+
+    def solve(self, start_idx: int = 0) -> Tuple[Solution, int]:
+        self.insert_first_nodes(start_idx)
+
+        # insert third node
+        position, vertex_idx, len_change = self.find_next_vertex()
+        self.insert_node(position, vertex_idx)
+        self.length += len_change
+
+        check = 0
+
+        while len(self.solution) < ceil(len(self.status)/2):
+            position, vertex_idx, len_change = self.find_next_vertex()
+            position2, _, regret = self.calculate_regret(position)
+            if len_change >= regret:
+                self.insert_node(position, vertex_idx)
+                self.length += len_change
+            else:
+                self.delete_node(position2)
+                if position > position2:
+                    position -= 1
+                self.insert_node(position, vertex_idx)
+                self.length -= regret
+                self.length += len_change
+            check += 1
+            if (check > 10000):
+                print('Broken')
+                return self.solution, 0
+
+        return self.solution, self.length
---
+++ tsp_router/local_search/two_opt_nodes_greedy.py
@@ -0,0 +1,151 @@
+import random
+from copy import deepcopy
+from typing import List, Optional, Tuple
+
+import numpy as np
+
+Candidate = Tuple[int, int]
+Tour = List[int]
+
+check_all = np.zeros((50,50))
+
+class TwoOptNodesGreedy:
+    """Local search tour improvement algorithm.
+    It consider every possible 2-edges swap,
+    swapping 2 edges when it results in an improved tour.
+    """
+
+    def __init(self) -> None:
+        pass
+
+    def swap_nodes(self, tour: Tour,
+                   candidate_i: int, candidate_j: int) -> Tour:
+        tmp = tour[candidate_i]
+        tour[candidate_i] = tour[candidate_j]
+        tour[candidate_j] = tmp
+        if candidate_i==0 and candidate_j==len(tour)-2:
+            tour[-1] = tour[0]
+        return tour
+
+    def find_best_node_in_tour(
+            self, tour: Tour, matrix: np.ndarray
+    ) -> Tuple[int, Optional[Candidate]]:
+        delta = 0
+        candidate = None
+        shuffled_indices = list(range(len(tour)-2))
+        random.shuffle(shuffled_indices)
+        check_all = np.zeros((50,50))
+        for i, node_ii in enumerate(shuffled_indices):
+            shuffled_indices_2 = shuffled_indices[i+1:].copy()
+            shuffled_indices_2.append(len(tour)-2)
+            for node_jj in shuffled_indices_2:
+                node_i, node_j = node_ii, node_jj
+                if node_j<node_i:
+                    node_i, node_j = node_j, node_i
+                check_all[node_i, node_j] += 1
+                if node_i==0:
+                    node_i_prev = len(tour)-2
+                else:
+                    node_i_prev = node_i - 1
+                if node_j==len(tour)-2:
+                    node_j_next = 0
+                else:
+                    node_j_next = node_j + 1
+                if node_i==0 and node_j==len(tour)-2:
+                    old_edges = matrix[tour[node_i], tour[node_i+1]] \
+                                + matrix[tour[node_j], tour[node_j-1]]
+                    new_edges = matrix[tour[node_i], tour[node_j-1]] \
+                                + matrix[tour[node_j], tour[node_i+1]]
+                elif node_i!=node_j-1:
+                    old_edges = matrix[tour[node_i_prev], tour[node_i]] \
+                                + matrix[tour[node_i], tour[node_i + 1]] \
+                                + matrix[tour[node_j - 1], tour[node_j]] \
+                                + matrix[tour[node_j], tour[node_j_next]]
+                    new_edges = matrix[tour[node_j], tour[node_i_prev]] \
+                                + matrix[tour[node_j], tour[node_i+1]] \
+                                + matrix[tour[node_j-1], tour[node_i]] \
+                                + matrix[tour[node_i], tour[node_j_next]]
+                else:
+                    old_edges = matrix[tour[node_i_prev], tour[node_i]] \
+                                + matrix[tour[node_j], tour[node_j_next]]
+                    new_edges = matrix[tour[node_j], tour[node_i_prev]] \
+                                + matrix[tour[node_i], tour[node_j_next]]
+                new_delta = new_edges - old_edges
+                if new_delta < delta:
+                    delta = new_delta
+                    candidate = (node_i, node_j)
+                    return delta, candidate
+        return 0, None
+
+    def find_best_node_insert(
+            self, tour: Tour, matrix: np.ndarray, all_vertices: Tour
+    ) -> Tuple[int, Optional[Candidate]]:
+        shuffled_indices = list(range(0, len(tour) - 2))
+        random.shuffle(shuffled_indices)
+        unused_vertices = list(set(all_vertices) - set(tour))
+        random.shuffle(unused_vertices)
+        delta = 0
+        candidate = None
+        for node_i in shuffled_indices:
+            if node_i==0:
+                node_i_prev = len(tour)-2
+            else:
+                node_i_prev = node_i - 1
+            for new_node in unused_vertices:
+                old_edges = matrix[tour[node_i_prev], tour[node_i]] \
+                            + matrix[tour[node_i], tour[node_i + 1]]
+                new_edges = matrix[tour[node_i_prev], new_node] \
+                            + matrix[new_node, tour[node_i + 1]]
+                new_delta = new_edges - old_edges
+                if new_delta < delta:
+                    delta = new_delta
+                    candidate = (node_i, new_node)
+                    return delta, candidate
+        return 0, None
+
+    def calc_length(self, tour: Tour, matrix: np.ndarray) -> int:
+        length = 0
+        for i in range(-1, len(tour) - 1):
+            length += matrix[tour[i], tour[i + 1]]
+        return length
+
+    def improve(self, tour: Tour, matrix: np.ndarray, all_vertices: Tour) -> Tour:
+        """We want to iterate until there is no improvement."""
+        i = 0
+        while True:
+            i += 1
+            if random.choice((0, 1)) == 1:
+                delta, candidate = self.find_best_node_in_tour(tour, matrix)
+                if delta != 0:
+                    tour = self.swap_nodes(tour, candidate[0], candidate[1])
+                    length = self.calc_length(tour, matrix)
+                    # print('l:', length)
+                else:
+                    delta, candidate = self.find_best_node_insert(
+                        tour, matrix, all_vertices
+                    )
+                    if delta != 0:
+                        tour[candidate[0]] = candidate[1]
+                        length = self.calc_length(tour, matrix)
+                        # print('l:', length)
+                    else:
+                        # print('t', check_all)
+                        return tour
+            else:
+                delta, candidate = self.find_best_node_insert(
+                    tour, matrix, all_vertices
+                )
+                if delta != 0:
+                    tour[candidate[0]] = candidate[1]
+                    length = self.calc_length(tour, matrix)
+                    # print('l:', length)
+                else:
+                    delta, candidate = self.find_best_node_in_tour(tour, matrix)
+                    if delta != 0:
+                        tour = self.swap_nodes(tour, candidate[0], candidate[1])
+                        length = self.calc_length(tour, matrix)
+                        # print('l:', length)
+                    else:
+                        # print('s', check_all)
+                        return tour
+
