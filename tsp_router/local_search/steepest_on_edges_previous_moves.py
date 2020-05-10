df26ae748bbf86bcdc45981424bc4ace40113d31
Piotr Wyrwiński <31487218+krigaree@users.noreply.github.com>
2020-05-10 14:02:59 +0200

    Steepest on edges previous moves without deepcopy fix

--- tsp_router/local_search/steepest_on_edges_previous_moves.py
+++ tsp_router/local_search/steepest_on_edges_previous_moves.py
@@ -1,6 +1,6 @@
 from collections import OrderedDict
 from copy import deepcopy
-from typing import List, Tuple, Dict
+from typing import List, Tuple, Dict, Union, Set
 
 import numpy as np
 
@@ -18,31 +18,26 @@ class SteepestOnEdgesPreviousMoves:
         pass
 
     def swap_nodes(self, node_i: int, node_j: int) -> None:
-        new_tour = deepcopy(self.tour)
-        new_tour[node_j] = self.tour[node_i]
-        new_tour[self.tour[node_i][0]][1] = node_j
-        new_tour[self.tour[node_i][1]][0] = node_j
-        del new_tour[node_i]
-        self.tour = new_tour
+        self.tour[self.tour[node_i][0]][1] = node_j
+        self.tour[self.tour[node_i][1]][0] = node_j
+        self.tour[node_j] = self.tour[node_i].copy()
+        del self.tour[node_i]
 
     def swap_edges(self, edge_i: Tuple[int, int],
                    edge_j: Tuple[int, int]) -> None:
-        new_tour = deepcopy(self.tour)
         if not self.check_direction_in_tour(edge_i):
             edge_i = edge_i[::-1]
             edge_j = edge_j[::-1]
-        node, node_after = edge_i[1], self.tour[edge_i[1]][1]  # next edge
+        node, node_after = edge_i[1], self.tour[edge_i[1]][1]
         while True:
-            new_tour[node][0] = self.tour[node][1]  # prev node is next node before
-            new_tour[node][1] = self.tour[node][0]  # next node is prev node
-            node, node_after = node_after, self.tour[node_after][1]  # next edge
+            self.tour[node] = self.tour[node][::-1]
+            node, node_after = node_after, self.tour[node_after][1]
             if node == edge_j[1]:
                 break
-        new_tour[edge_i[0]][1] = edge_j[0]
-        new_tour[edge_i[1]][1] = edge_j[1]
-        new_tour[edge_j[0]][0] = edge_i[0]
-        new_tour[edge_j[1]][0] = edge_i[1]
-        self.tour = new_tour
+        self.tour[edge_i[0]][1] = edge_j[0]
+        self.tour[edge_i[1]][1] = edge_j[1]
+        self.tour[edge_j[0]][0] = edge_i[0]
+        self.tour[edge_j[1]][0] = edge_i[1]
 
     def get_next_edge(self, edge: Tuple[int, int]) -> Tuple[int, int]:
         return edge[1], self.tour[edge[1]][1]
@@ -58,7 +53,8 @@ class SteepestOnEdgesPreviousMoves:
             j = 0
             while tmp_first != second_edge:
                 j += 1
-                if (first_edge, second_edge) not in self.edge_used_moves and (second_edge, first_edge) not in self.edge_used_moves:
+                if ((first_edge, second_edge) not in self.edge_used_moves and
+                        (second_edge, first_edge) not in self.edge_used_moves):
                     sum_old_edges = self.matrix[first_edge] + \
                                     self.matrix[second_edge]
                     new_edge_i = (self.tour[first_edge[0]][1],
@@ -75,7 +71,7 @@ class SteepestOnEdgesPreviousMoves:
             first_edge = self.get_next_edge(first_edge)
             second_edge = self.get_next_edge(first_edge)
 
-    def find_best_node_insert(self):
+    def find_best_node_insert(self) -> None:
         unused_vertices = list(set(self.all_vertices) - set(self.tour))
         for node, (before, after) in self.tour.items():
             for new_node in unused_vertices:
@@ -83,10 +79,10 @@ class SteepestOnEdgesPreviousMoves:
                 second_edge = (node, after)
                 new_fist_edge = (before, new_node)
                 new_second_edge = (new_node, after)
-                if ((before, after), (node, new_node)) not in self.node_used_moves \
-                    and ((before, after), (new_node, node)) not in self.node_used_moves \
-                    and ((after, before), (node, new_node)) not in self.node_used_moves \
-                    and ((after, before), (new_node, node)) not in self.node_used_moves:
+                if (((before, after), (node, new_node)) not in self.node_used_moves and
+                        ((before, after), (new_node, node)) not in self.node_used_moves and
+                        ((after, before), (node, new_node)) not in self.node_used_moves and
+                        ((after, before), (new_node, node)) not in self.node_used_moves):
                     old_edges = self.matrix[first_edge] + self.matrix[
                         second_edge]
                     new_edges = self.matrix[new_fist_edge] + self.matrix[
@@ -94,14 +90,12 @@ class SteepestOnEdgesPreviousMoves:
                     delta = new_edges - old_edges
                     if delta < 0:
                         self.candidates[(node, new_node)] = delta
-                    # self.node_used_moves.add((first_edge, second_edge))
                     self.node_used_moves.add(((before, after), (node, new_node)))
 
     @staticmethod
     def convert_tour(tour: Tour) -> Dict[int, List[int]]:
         new_tour = {}
         for i in range(len(tour)):
-            # Edge = (node_before, node_after)
             new_tour[tour[i - 1]] = [tour[i - 2], tour[i]]
         return new_tour
 
@@ -124,15 +118,14 @@ class SteepestOnEdgesPreviousMoves:
 
     def improve(self, tour: Tour, matrix: np.ndarray,
                 all_vertices: Tour) -> Tour:
-        """We want to iterate until there is no improvement."""
         self.tour = self.convert_tour(tour)
         self.matrix = matrix
         self.all_vertices = all_vertices
-        break_flag = True  # If new candidate is found don't break loop
+        break_flag = True
         i = 0
-        self.candidates = {}
-        self.edge_used_moves = set()
-        self.node_used_moves = set()
+        self.candidates: Dict[Union[Tuple[Tuple[int, int], Tuple[int, int]], Tuple[int, int]], float] = {}
+        self.edge_used_moves: Set[Tuple[Tuple[int, int], Tuple[int, int]]] = set()
+        self.node_used_moves: Set[Tuple[Tuple[int, int], Tuple[int, int]]] = set()
         while break_flag:
             i += 1
             break_flag = False
@@ -140,29 +133,22 @@ class SteepestOnEdgesPreviousMoves:
             self.find_best_node_insert()
             ordered_candidates = OrderedDict(
                 sorted(self.candidates.items(), key=lambda t: t[1]))
-            # print(f"\r{list(ordered_candidates.items())[0]} {len(ordered_candidates)} {len(self.edge_used_moves)} {len(self.node_used_moves)}", end="")
-            # print(f"\r{len(ordered_candidates)} {len(self.edge_used_moves)} {len(self.node_used_moves)}", end="")
-
             for candidate in ordered_candidates:
-                # Candidate is an edge
                 if type(candidate[0]) is tuple:
                     if self.check_edge_in_tour(candidate[0]) \
                             and self.check_edge_in_tour(candidate[1]):
                         if not (self.check_direction_in_tour(candidate[0]) \
                                 ^ self.check_direction_in_tour(candidate[1])):
-                                self.swap_edges(candidate[0], candidate[1])
+                                self.swap_edges(candidate[0], candidate[1])  # type: ignore
                                 break_flag = True
                                 self.candidates.pop(candidate)
-                                # break
                     else:
                         self.candidates.pop(candidate)
                 else:
                     if candidate[0] in self.tour and candidate[1] not in self.tour:
-                        self.swap_nodes(candidate[0], candidate[1])
+                        self.swap_nodes(candidate[0], candidate[1])  # type: ignore
                         break_flag = True
                         self.candidates.pop(candidate)
-                        # break
                     else:
                         self.candidates.pop(candidate)
-
         return self.invert_tour(self.tour)
