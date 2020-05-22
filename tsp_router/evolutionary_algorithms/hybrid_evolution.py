e31d5eb93bbf7a596386f283ccb7162ae84b548b
Piotr Wyrwiński <31487218+krigaree@users.noreply.github.com>
2020-05-22 15:58:00 +0200

    Recombination added.

--- tsp_router/evolutionary_algorithms/hybrid_evolution.py
+++ tsp_router/evolutionary_algorithms/hybrid_evolution.py
@@ -1,11 +1,18 @@
-from typing import List, Tuple
+import os
+from copy import deepcopy
+from typing import List, Tuple, Dict
 import random
+
+from tsp_router.tour import Tour
+
+random.seed(0)
 import numpy as np
+np.random.seed(0)
 from time import time
 
-from ..local_search.steepest_on_edges import SteepestOnEdges
+from tsp_router.local_search.steepest_on_edges import SteepestOnEdges
+from tsp_router.utils.loader import Loader
 
-Tour = List[int]
 Candidate = Tuple[int, int]
 
 
@@ -16,7 +23,7 @@ class HybridEvolution:
         self.matrix = matrix
         self.all_vertices = list(range(self.matrix.shape[0]))
 
-    def solve(self, max_time: int) -> Tuple[int, Tour]:
+    def solve(self, max_time: int) -> Tuple[int, List[int]]:
         pop = self.generate_population(population_size=20)
         start_time = time()
         while max_time > (time() - start_time):
@@ -43,7 +50,65 @@ class HybridEvolution:
         return population[rand_choices[0]], population[rand_choices[1]]
 
     def recombine(self):
-        pass
+        def gen_rand(tour_len):
+            return random.sample(list(range(tour_len)), int(np.ceil(tour_len)))
+        tour_len = 100
+        parent1, parent2 = gen_rand(tour_len), gen_rand(tour_len)
+        print(parent1, parent2)
+        parent1 = Tour.convert_tour(parent1)
+        parent2 = Tour.convert_tour(parent2)
+        sub_tours = []
+        while parent1 and parent2:
+            print(len(parent1), len(parent2))
+            sub_tour = []
+            for k1, v1 in parent1.items():
+                if not sub_tour:
+                    for k2, v2 in parent2.items():
+                        print(k1, k2)
+                        if k1 == k2:
+                            sub_tour.append(k1)
+                            print(k1)
+                            prev1, next1 = parent1[k1]
+                            prev2, next2 = parent2[k1]
+                            break
+                else:
+                    break
+            if sub_tour:
+                while True:
+                    in_len = len(sub_tour)
+                    print("TU:", sub_tour)
+                    if prev1 == prev2:
+                        if prev1 not in sub_tour:
+                            sub_tour.insert(0, prev1)
+                            prev1, prev2 = parent1[prev1][0], parent2[prev1][0]
+                    if next1 == next2:
+                        if next1 not in sub_tour:
+                            sub_tour.append(next1)
+                            next1, next2 = parent1[next1][1], parent2[next1][1]
+                    if len(sub_tour) == in_len:
+                        break
+                print(sub_tour)
+                for s in sub_tour:
+                    del parent1[s]
+                    del parent2[s]
+                sub_tours.append(sub_tour)
+                # break
+            else:
+                for k1 in parent1:
+                    sub_tours.append([k1])
+                for k2 in parent2:
+                    sub_tours.append([k2])
+                parent1, parent2 = {}, {}
+                break
+
+        print(parent1, parent2)
+        print("SUB TOURS:", sub_tours)
+        # print("FINAL SUB TOURS:", final_sub_tours)
+
+
+
+
+
 
     def check_if_improves(self):
         pass
@@ -53,3 +118,10 @@ class HybridEvolution:
 
     def find_best_solution(self):
         pass
+
+if __name__ == "__main__":
+    loader = Loader("../../data/kroA200.tsp")
+    vertices = loader.load_vertices()
+    matrix = loader.calculate_matrix(vertices)
+    solver = HybridEvolution(matrix)
+    solver.recombine()
--- tsp_router/tour.py
+++ tsp_router/tour.py
@@ -1,4 +1,16 @@
+from typing import List, Dict
+
+Tour = List[int]
+
+
 class Tour:
 
     def __init__(self):
         pass
+
+    @staticmethod
+    def convert_tour(tour: Tour) -> Dict[int, List[int]]:
+        new_tour = {}
+        for i in range(len(tour)):
+            new_tour[tour[i - 1]] = [tour[i - 2], tour[i]]
+        return new_tour
